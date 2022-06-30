5ad9c00f620a93b472b5df84db90db4c7b3f45fb
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-06-30 06:11:54 +0100

    added request body parser for json

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
+const { config } = require('dotenv')
 const connect = require('./config/database').connect
 
 const routes = require('./routes/todoRoutes')
+config()
 connect()
-
 const PORT = parseInt(process.env.PORT) 
 
 
 const app = express() 
 
+app.use(express.json())
+
 app.use("/", routes) 
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
