cec6dd8b6ea5e57f0ee14e0d3c6296310260fa6a
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-06-30 06:08:00 +0100

    made mongoose uri an environment variable

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose')
+const { config } = require('dotenv')
+
+config()
 
 exports.connect = (uri) => {
     try {
-        mongoose.connect(uri || 'mongodb://localhost/')
-        console.log('Connected to MongoDB')
+        mongoose
+            .connect(uri || process.env.MONGO_DB_LOCAL)
+            .then(() => console.log('Connected to MongoDB'))
+
     } catch (error) {
         console.error(error.message)
-        
+
     }
 }
