1277ad65960a6e08b7d7a08a17c55b352b56a841
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-07-18 15:07:06 +0100

    got rid of security vulnerabilities

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,91 +1,104 @@
-const Task = require('../models/task')
+const Task = require("../models/task");
 
-exports.getAllTasks = async(req, res) => {
-    try {
-        let tasks = await Task.find()
-        res.status(200).json({
-            message: 'Tasks retrieved',
-            taskCount: tasks.length,
-            tasks: tasks,
-            
-        })
-    } catch (error) {
-        res.status(500).json({ 
-            message: 'Internal server error',
-            error: error.message
-        })
-    }
-}
+exports.getAllTasks = async (req, res) => {
+  try {
+    let tasks = await Task.find();
+    res.status(200).json({
+      message: "Tasks retrieved",
+      taskCount: tasks.length,
+      tasks: tasks,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+    console.log(error.message);
+  }
+};
 
-exports.getTask = async(req, res) => {
-    try {
-        let task_id = req.params.id
-        let task = await Task.findById(task_id)
-        if(!task) {
-            res.status(404).json({
-                message: 'Task not found'
-            })
-        }
-        res.status(200).json(task)
-    } catch (error) {
-        res.status(500).json(error)        
+exports.getTask = async (req, res) => {
+  try {
+    let task_id = req.params.id;
+    let task = await Task.findById(task_id);
+    if (!task) {
+      res.status(404).json({
+        message: "Task not found",
+      });
     }
-}
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+    console.log(error.message);
+  }
+};
 
-exports.addTask = async(req, res) => {
-    try {
-        let task = req.body
-        let createdTask = await Task.create(task)
-        console.log(task)
-        if(!createdTask) {
-            return res.status(400).json({
-                message: 'Task not created',
-                error: error.message
-            })
-        }
-        console.log(createdTask) 
-        res.status(200).json({
-            message: 'Task created',
-            task: createdTask
-        })
-        
-    } catch (error) {
-        res.status(500).json(error)        
+exports.addTask = async (req, res) => {
+  try {
+    let task = req.body;
+    let createdTask = await Task.create(task);
+    console.log(task);
+    if (!createdTask) {
+      res.status(400).json({
+        message: "Task not created",
+        error: "An error occured",
+      });
+      console.log(error.message);
     }
-}
+    console.log(createdTask);
+    res.status(200).json({
+      message: "Task created",
+      task: createdTask,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+    console.log(error.message);
+  }
+};
 
-exports.updateTask = async(req, res) => {
-    try {
-        let task_id = req.params.id
-        let updatedTask = await Task.findByIdAndUpdate(task_id, req.body, {new: true})
-        if(!updatedTask) {
-            return res.status(400).json({
-                message: 'Task not updated'
-            })
-        }
-        res.status(200).json({
-            message: 'Task updated',
-            updatedTask
-        })
-    } catch (error) {
-        res.status(500).json(error)                
+exports.updateTask = async (req, res) => {
+  try {
+    let task_id = req.params.id;
+    let updatedTask = await Task.findByIdAndUpdate(task_id, req.body, {
+      new: true,
+    });
+    if (!updatedTask) {
+      return res.status(400).json({
+        message: "Task not updated",
+      });
     }
-}
+    res.status(200).json({
+      message: "Task updated",
+      updatedTask,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+    console.log(error.message);
+  }
+};
 
-exports.deleteTask = async(req, res) => {
-    try {
-        let task_id = req.params.id
-        let deletedTask = await Task.findByIdAndDelete(task_id)
-        if(!deletedTask) {
-            return res.status(400).json({
-                message: 'Task not deleted'
-            })  
-        }
-        res.status(200).json({
-            message: 'Task deleted',
-            deletedTask
-        })
-    } catch (error) {
-        res.status(500).json(error)                
+exports.deleteTask = async (req, res) => {
+  try {
+    let task_id = req.params.id;
+    let deletedTask = await Task.findByIdAndDelete(task_id);
+    if (!deletedTask) {
+      return res.status(400).json({
+        message: "Task not deleted",
+      });
     }
-}
+    res.status(200).json({
+      message: "Task deleted",
+      deletedTask,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+    console.log(error.message);
+  }
+};
